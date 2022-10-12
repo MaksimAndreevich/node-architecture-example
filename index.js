aa41e4e1a74799d4344dd2e23f69db2802658bdf
Maksim <maksim.e@mail.ru>
2022-10-12 16:06:10 +0300

    router express

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 import express from "express";
+import { userRouter } from "./users/users.js";
 
 const port = 8000;
 const app = express();
 
-app.get("/hello", (req, res) => {
-  res.send("Hello World!");
+app.use((req, res, next) => {
+  console.log(`Time ${Date.now()}`);
+  next();
+});
+
+app.all("/hello", (req, res, next) => {
+  console.log("Work app.all");
+  next();
+});
+
+app.use("/users", userRouter);
+
+const callback = (req, res, next) => {
+  console.log("Wokr callback");
+  next();
+};
+
+app.get("/hello", [
+  callback,
+  //   callback,
+  //   callback,
+  (req, res) => {
+    res.send("Hello World!");
+  },
+]);
+
+app.get("/redirect", (req, res) => {
+  res.redirect(301, "https://yandex.ru");
 });
 
 app.listen(port, () =>

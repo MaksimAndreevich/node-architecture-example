d8ca809c1e9a901f7bb5146839a1fe88b30a9cf9
Maksim <maksim.e@mail.ru>
2022-11-01 12:46:38 +0300

    route middlewares

--- src/common/base.controller.ts
+++ src/common/base.controller.ts
@@ -32,8 +32,12 @@ export abstract class BaseController {
 	protected bindRoutes(routes: IRoute[]): void {
 		for (const route of routes) {
 			this.logger.log(`[${route.method}] ${route.path}`);
+
+			const middleware = route.middlewares?.map((m) => m.execute.bind(m));
+
 			const handler = route.func.bind(this);
-			this._router[route.method](route.path, handler);
+			const pipeline = middleware ? [...middleware, handler] : handler;
+			this._router[route.method](route.path, pipeline);
 		}
 	}
 }
